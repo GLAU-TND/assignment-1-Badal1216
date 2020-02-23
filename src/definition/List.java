edbeec55175e5cdb796b6023a4c82c730df8bda9
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-02-23 10:31:15 +0530

    create the getNode() method in list class

--- src/definition/List.java
+++ src/definition/List.java
@@ -15,7 +15,26 @@ public class List<E> implements ListADT<E> {
     }
 
     private void addAfter(Node<E> node, E item) {
+        node.next = new Node<E>(item, node.next);
+        size++;
+    }
 
+    public void add(int index, E item) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        else if (index == 0) {
+            addFirst(item);
+        } else
+            Node<E> temp = getNode(index - 1);
+
+    }
+
+    private Node<E> getNode(int index) {
+        Node<E> response = head;
+        for (int i = 0; i < index; i++) {
+            response = response.next;
+        }
+        return response;
     }
 
 
